c560858c770eac7ba17e1cc298c84f715e57cb12
Taiki Nishino <tak.nshno.dic@gmail.com>
2023-03-28 11:35:48 +0900

    feat: Add form varidation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,8 +8,12 @@ class PostsController < ApplicationController
   end
 
   def create
-    Post.create(post_params)
-    redirect_to new_post_path
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to new_post_path, notice: "つぶやきを投稿しました！"
+    else
+      render :new
+    end
   end
 
   private
