80d592f34b540a3c57d26fbe43b1f5ac123eada8
Taiki Nishino <tak.nshno.dic@gmail.com>
2023-03-28 15:16:49 +0900

    feat: Add tweet delete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:edit, :update, :destroy]
+
   def index
     @posts = Post.all
   end
@@ -17,11 +19,9 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
     if @post.update(post_params)
       redirect_to posts_path, notice: "つぶやきを編集しました！"
     else
@@ -29,9 +29,18 @@ class PostsController < ApplicationController
     end
   end
 
+  def destroy
+    @post.destroy
+    redirect_to posts_path, notice:"つぶやきを削除しました！"
+  end
+
   private
 
   def post_params
     params.require(:post).permit(:content)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
